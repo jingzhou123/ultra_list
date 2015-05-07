4c4301da282beb2979fdf397da4dfaacc197f65c
jiwang <jiwang@alibaba-inc.com>
2015-05-07 23:56:44 +0800

    a list with add & rm

---
+++ basic_list/app.js
@@ -0,0 +1,28 @@
+var mockPost = function(path) {
+  var data = {};
+  return $.when(data);
+};
+var ListViewModel = function() {
+  var path = 'list.json';
+  var that = this;
+  this.list = ko.observableArray([]);
+  this.newItemName = ko.observable('');
+  this.addItem = function() {
+    var that = this;
+    mockPost(path).then(function(res) {
+      that.list.push({itemName: that.newItemName()});
+    });
+  };
+
+  this.rmItem = function(item) {
+    mockPost(path).then(function(res) {
+      that.list.remove(item);
+    });
+  };
+
+  $.get(path).then(function(res) {
+    that.list(res);
+  });
+};
+
+ko.applyBindings(new ListViewModel());
